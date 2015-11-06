046355ae68010a4685c8bcf489a4c83700650891
JelizavetaK <jelizaveta.konstantinova@kcl.ac.uk>
2015-11-06 19:01:37 +0000

    DAQ software for fingertip (tactile and proximity) and wrist sensors

--- squirrel_sensing_node/include/squirrel_sensing_node/sensing_drivers.h
+++ squirrel_sensing_node/include/squirrel_sensing_node/sensing_drivers.h
@@ -1,18 +1,25 @@
 #ifndef SENSING_DRIVERS
 #define SENSING_DRIVERS
 
-
+#include <string>
+#include <vector>
+#include <queue>
+#include <FT17/FT17Interface.h>
 
 class Driver{   //abstract class: everything in common across sensors is here
 //nothing here (no private data)
 protected:
+    std::string m_portname; //name of the arduino port
+    int m_fileDesc;         //file descriptor for termios
 
     //config matrix (filename?)
 
-    virtual bool setup();   //assuming setup is equal for 2 sensros on 3
+    virtual bool setup();   //assuming setup is equal for 2 sensors on 3
+
 
 public:
-    virtual double** read()=0;  //this function reads the data from the sensors and returns a c-matrix (double[][])
+    virtual std::vector<double>* readData()=0;  //this function reads the data from the sensors and returns a vector (double[][])
+    virtual void flush();
 
 };
 
@@ -21,22 +28,74 @@ public:
 
 class Tactile : public Driver{
 
+    static const double A_TACT;
+    static const double B_TACT;
+    static const double C_TACT;
+    static const double D_TACT;
+
+    static const double A_PROX;
+    static const double B_PROX;
+    static const double C_PROX;
+    static const double D_PROX;
+
+    static const int NUM_TACT; //num of tactile sensors
+    static const int NUM_PROX; //num of proximity sensors
+    static const int NUM_VALS; //num of readings
+
+    static const int NUM_HISTORY_VALS; //number of readings to use for stationary check
+    static const int NUM_BIAS_VALS; //number of values to accumulate for calculating the bias
+    static const double STATIONARY_TACTILE_THREASHOLD; //voltage threashold, if passed data is stationary
+    static const double STATIONARY_PROXIMITY_THREASHOLD; //voltage threashold, if passed data is stationary
+
+    std::vector<double> divider; //vecotr containing voltage divider numbers read from file
+    std::vector<double> history_val_tact; //sum previous voltage values
+    std::vector<std::queue<double>* > history_tact; //list of previous voltage values
+    std::vector<double> history_val_prox; //sum previous voltage values
+    std::vector<std::queue<double>* > history_prox; //list of previous voltage values
+    std::vector<std::vector<double> > mean; //first 10 values used for calculating the bias
+
+    double bias(const int idx,const double val);
+    double convertTact(const double num);
+    double convertProx(const double num);
+    bool isStationaryTact(const double val,const int idx);
+    bool isStationaryProx(const double val,const int idx);
+
 public:
-    virtual double** read();
+    Tactile(const std::string& portname);
+    ~Tactile();
+    virtual std::vector<double>* readData();
 
 };
 
-class Proximity : public Driver{
+//not used for now
+/*class Proximity : public Driver{
 
 public:
-    virtual double** read();
+    virtual std::vector<double>* readData();
 
-};
+};*/
 
 class Wrist : public Driver{
 
+    ft_data ft_bc_data;
+    FT17Interface* ft17;
 public:
-    virtual double** read();
+    enum WristData
+    {
+        ForceX,
+        ForceY,
+        ForceZ,
+        TorqueX,
+        TorqueY,
+        TorqueZ,
+        Timestamp,
+        WristDataNum
+    };
+
+    Wrist();
+    ~Wrist();
+
+    virtual std::vector<double>* readData();
 
 };
 
--- squirrel_sensing_node/src/node.cpp
+++ squirrel_sensing_node/src/node.cpp
@@ -1,4 +1,13 @@
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
 
 #include <ros/ros.h>
 #include <std_msgs/Float64MultiArray.h>
@@ -14,24 +23,32 @@ using namespace std;
 const double SensingNode::pause=1000.0;    //this might be became a constructor parameter
 
 //consider instantiating everything in a configure() function instead of the constructor
-SensingNode::SensingNode(const std::string& name){
+SensingNode::SensingNode(const std::string& name, const std::string& portname){
 
     this->name=name;
 
-    //IMPORTANT: is operator= implemented by NodeHandle?
-    tactile_pub=(node.advertise<std_msgs::Float64MultiArray>(name,1));    //1 is maximum number of messages sent before going in overflow
-    proximity_pub=(node.advertise<std_msgs::Float64MultiArray>(name,1));    //1 is maximum number of messages sent before going in overflow
-    wrist_pub=(node.advertise<std_msgs::Float64MultiArray>(name,1));    //1 is maximum number of messages sent before going in overflow
+    tactile_pub=(node.advertise<std_msgs::Float64MultiArray>("fingertips",1));    //1 is maximum number of messages sent before going in overflow
+    //proximity_pub=(node.advertise<std_msgs::Float64MultiArray>("pro",1));    //1 is maximum number of messages sent before going in overflow
+    wrist_pub=(node.advertise<std_msgs::Float64MultiArray>("wrist",1));    //1 is maximum number of messages sent before going in overflow
 
     loop_rate=new Rate(pause);
 
-    //sensor=new Sensor();      //to implement
+    sensor=new Tactile(portname); //"/dev/ttyACM0");      //the port name will be given from command line
+#ifdef _FT17_AVAIL
+    wrist=new Wrist();
+#else
+    wrist=NULL;
+#endif
 }
 
 SensingNode::~SensingNode(){
 
     if(sensor!=NULL){
         delete sensor;
+    }
+
+    if(wrist!=NULL){
+        delete wrist;
     }
 
     if(loop_rate!=NULL){
@@ -43,17 +60,44 @@ SensingNode::~SensingNode(){
 void SensingNode::run(){ //this function will make the node loop as long as ros::ok() is true
 
     //here can be added every function needed to set up the sensor execution (i.e. config file reading)
-    //sensor->setup();
 
+    sensor->flush();
     while(ros::ok()){
         //read from sensor
 
-        //fill in msg
+        //proximity and tactile
+        vector<double>* vals=sensor->readData();
 
-        //ROS_INFO("Broadcasting...")
-        //tactile_pub->publish(msg);
+        std_msgs::Float64MultiArray msg;
+        msg.data.resize(vals->size());
+
+        //fill in msg
+        for(int i=0;i<vals->size();i++){
+            msg.data[i]=vals->at(i);
+        }
+
+#ifdef _FT17_AVAIL
+        //wrist
+        vector<double>* wriVals=wrist->readData();
+
+        std_msgs::Float64MultiArray msgWri;
+        msgWri.data.resize(wriVals->size());
+
+        //fill in msg
+        for(int i=0;i<wriVals->size();i++){
+            msgWri.data[i]=wriVals->at(i);
+        }
+#endif
+
+        //ROS_INFO("Broadcasting...");
+        tactile_pub.publish(msg);
+#ifdef _FT17_AVAIL
+        wrist_pub.publish(msgWri);
+#endif
 
         ros::spinOnce();
+
+        delete vals;
 
         loop_rate->sleep();
     }
