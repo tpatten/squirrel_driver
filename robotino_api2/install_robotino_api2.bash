e17c0327d209eef039f5ee057f7c8ed70963e3db
Marc Riedlinger <Marc.Riedlinger@ipa.fraunhofer.de>
2016-09-01 15:18:02 +0200

    fix

--- robotino_api2/install_robotino_api2.bash
+++ robotino_api2/install_robotino_api2.bash
@@ -8,8 +8,7 @@ if [ -e $INSTALLED_DIR ]; then
 	echo  "robotino-api2 is already installed."
 else
 	wget -qO - http://packages.openrobotino.org/keyFile | sudo apt-key add -
-	sudo su
-	echo "deb http://packages.openrobotino.org/trusty trusty main" > /etc/apt/sources.list.d/openrobotino.list
+	sudo echo "deb http://packages.openrobotino.org/trusty trusty main" > /etc/apt/sources.list.d/openrobotino.list
 	sudo apt-get update
 	sudo apt-get install rec-rpc robotino-common robotino_daemons robotino-api2 robotino-examples robotino3-firmware
 	touch $INSTALLED_DIR
