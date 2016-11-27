04f88f3597b04420f280112371bf8723293de5d7
Philipp Zech <philipp.zech@uibk.ac.at>
2016-11-27 14:31:41 +0100

    idea for autocalibration

--- squirrel_softhand/src/softhand_controller.cpp
+++ squirrel_softhand/src/softhand_controller.cpp
@@ -4,8 +4,10 @@
 #include "math.h"
 #include "../include/qbmove_communications.h"
 
-int device_id;
-comm_settings comm_settings_t;
+struct global_args {
+    int device_id;
+    comm_settings comm_settings_t;
+} global_args;
 
 bool actuate(squirrel_manipulation_msgs::SoftHandGrasp::Request & req,
              squirrel_manipulation_msgs::SoftHandGrasp::Response & res)
@@ -14,8 +16,8 @@ bool actuate(squirrel_manipulation_msgs::SoftHandGrasp::Request & req,
     inputs[0] = ceil(req.position*17000.0);
     ROS_INFO("Position: %d", inputs[0]);
     inputs[1] = 0;    
-    commSetInputs(&comm_settings_t, device_id, inputs);
-    if(comm_settings_t.file_handle == INVALID_HANDLE_VALUE)
+    commSetInputs(&global_args.comm_settings_t, global_args.device_id, inputs);
+    if(global_args.comm_settings_t.file_handle == INVALID_HANDLE_VALUE)
     {
         ROS_ERROR("Couldn't move hand!");
         res.success = false;
@@ -23,6 +25,23 @@ bool actuate(squirrel_manipulation_msgs::SoftHandGrasp::Request & req,
     }
     res.success = true;
     return true;    
+}
+
+bool do_calibration()
+{
+    // CONFIGURE LIMITS
+    int limits[4];
+    limits[0] = 0;
+    limits[1] = 17000;
+    limits[2] = 0;
+    limits[3] = 0;
+    int speed = 200;
+    int repetitions = 15;
+    commSetParam(&global_args.comm_settings_t, global_args.device_id, PARAM_POS_LIMIT, limits, 4);
+    commCalibrate(&global_args.comm_settings_t, global_args.device_id);
+    commHandCalibrate(&global_args.comm_settings_t, global_args.device_id, speed, repetitions);
+    ROS_WARN("Not yet implemented!");
+    return true;
 }
 
 int main(int argc, char **argv)
@@ -36,29 +55,48 @@ int main(int argc, char **argv)
     ros::param::param<std::string>("port", port, ports[0]);
     ROS_INFO("Selected port: %s", port.c_str());
 
-    int device_id;
-    ros::param::param<int>("device_id", device_id, 1);
-    ROS_INFO("Device ID: %d", device_id);
+    ros::param::param<int>("device_id", global_args.device_id, 1);
+    ROS_INFO("Device ID: %d", global_args.device_id);
 
-    openRS485(&comm_settings_t, port.c_str());
-    if(comm_settings_t.file_handle == INVALID_HANDLE_VALUE)
+    // CONNECT
+    openRS485(&global_args.comm_settings_t, port.c_str());
+    if(global_args.comm_settings_t.file_handle == INVALID_HANDLE_VALUE)
     {
         ROS_ERROR("Couldn't connect to serial port: %p", port.c_str());
         return EXIT_FAILURE;
     } else {
         ROS_INFO("Connected to %s.", port.c_str());
-    }
-    
-    commActivate(&comm_settings_t, device_id, 1);    
+    }    
+    commActivate(&global_args.comm_settings_t, global_args.device_id, 1);    
     ros::ServiceServer service = n.advertiseService("softhand_grasp", actuate);
     ROS_INFO("Ready to grasp.");
-    
+
+    // CALIBRATE
+    bool calibrate;
+    ros::param::param<bool>("calibrate", calibrate, true);
+    if(calibrate)
+    {
+        ROS_INFO("Performing autocalibration.");
+        if(do_calibration())
+        {
+            ROS_INFO("Autocalibration done.");
+        } else
+        {
+            ROS_ERROR("Autocalibration failed!");
+            return EXIT_FAILURE;
+        }        
+    } else {
+        ROS_INFO("Skipping autocalibration.");
+    }
+
+    // SERVICE
     while(ros::ok()){
         ros::spinOnce();
     }
 
-    commActivate(&comm_settings_t, device_id, 0);
-    closeRS485(&comm_settings_t);
+    // SHUTDOWN
+    commActivate(&global_args.comm_settings_t, global_args.device_id, 0);
+    closeRS485(&global_args.comm_settings_t);
 
     return EXIT_SUCCESS;
 }
