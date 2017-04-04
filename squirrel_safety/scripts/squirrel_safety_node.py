9fd79e716daf0861624a68b2fe05f719319d2187
Philipp Zech <philipp.zech@uibk.ac.at>
2017-04-04 21:15:36 +0200

    extended launch file to easily change topic names

--- squirrel_safety/scripts/squirrel_safety_node.py
+++ squirrel_safety/scripts/squirrel_safety_node.py
@@ -8,7 +8,10 @@ from squirrel_safety import safety
 if __name__ == '__main__':
     try:
         rospy.init_node('squirrel_safety_node')
-        safety_ = safety.SquirrelSafety()
+        airskin_topic = rospy.get_param('~airskin_topic')
+        bumper_topic = rospy.get_param('~bumper_topic')
+        wrist_topic = rospy.get_param('~wrist_topic')
+        safety_ = safety.SquirrelSafety(airskin_topic, bumper_topic, wrist_topic)
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
--- squirrel_safety/src/squirrel_safety/safety.py
+++ squirrel_safety/src/squirrel_safety/safety.py
@@ -11,15 +11,15 @@ from squirrel_manipulation_msgs.msg import BlindGraspFeedback
 from visualization_msgs.msg import Marker
 
 class SquirrelSafety(object):
-    def __init__(self):
+    def __init__(self, airskin_topic, bumper_topic, wrist_topic):
         while rospy.get_time() == 0.0:
             pass
 
         rospy.loginfo(rospy.get_caller_id() + ': starting up')
 
-        self.wrist_sub = rospy.Subscriber('/wrist/wrist_bumper', Bool, self.wrist_callback)
-        self.bumper_sub = rospy.Subscriber('/bumper', Bool, self.bumper_callback)
-        self.airskin_sub = rospy.Subscriber('arm_bumper', Bool, self.airskin_callback)
+        self.wrist_sub = rospy.Subscriber(wrist_topic, Bool, self.wrist_callback)
+        self.bumper_sub = rospy.Subscriber(bumper_topic, Bool, self.bumper_callback)
+        self.airskin_sub = rospy.Subscriber(airskin_topic, Bool, self.airskin_callback)
         self.safety_pub = rospy.Publisher('/squirrel_safety', Safety, queue_size=10)
 
 
