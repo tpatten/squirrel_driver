8e6e590d9022620e121a758c258cba4b238ac388
Philipp Zech <philipp.zech@uibk.ac.at>
2017-04-05 10:17:37 +0200

    minor fixes

--- squirrel_safety/src/squirrel_safety/safety.py
+++ squirrel_safety/src/squirrel_safety/safety.py
@@ -3,13 +3,6 @@ import rospy
 from squirrel_safety_msgs.msg import Safety
 from std_msgs.msg import Bool
 
-from squirrel_manipulation_msgs.srv import SoftHandGrasp
-from geometry_msgs.msg import Pose, PoseStamped
-from squirrel_manipulation_msgs.msg import BlindGraspAction
-from squirrel_manipulation_msgs.msg import BlindGraspResult
-from squirrel_manipulation_msgs.msg import BlindGraspFeedback
-from visualization_msgs.msg import Marker
-
 class SquirrelSafety(object):
     def __init__(self, airskin_topic, bumper_topic, wrist_topic):
         while rospy.get_time() == 0.0:
@@ -22,28 +15,37 @@ class SquirrelSafety(object):
         self.airskin_sub = rospy.Subscriber(airskin_topic, Bool, self.airskin_callback)
         self.safety_pub = rospy.Publisher('/squirrel_safety', Safety, queue_size=10)
 
+        rate = rospy.Rate(10)
+        self.safety_msg = Safety()
+        self.safety_msg.emergency_state = False
+        self.safety_msg.airskin_stop = False
+        self.safety_msg.wrist_stop = False
+        self.safety_msg.bumper_stop = False
+
+        while not rospy.is_shutdown():
+            if self.safety_msg.wrist_stop or self.safety_msg.bumper_stop or self.safety_msg.airskin_stop:
+                self.safety_msg.emergency_state = True
+            self.safety_pub.publish(self.safety_msg)
+            self.safety_msg.emergency_state = False
+            rate.sleep()
+
 
     def airskin_callback(self, msg):
-      if msg:
-        safety_msg = Safety()
-        safety_msg.emergency_state = True
-        safety_msg.airskin_stop = True
-        self.safety_pub.publish(safety_msg)
+        if msg:
+            self.safety_msg.airskin_stop = True
+        else:
+            self.safety_msg.airskin_stop = False
 
 
     def wrist_callback(self, msg):
-      if msg:
-        safety_msg = Safety()
-        safety_msg.emergency_state = True
-        safety_msg.wrist_stop = True
-        self.safety_pub.publish(safety_msg)
+        if msg:
+            self.safety_msg.wrist_stop = True
+        else:
+            self.safety_msg.wrist_stop = False
 
 
     def bumper_callback(self, msg):
-      if msg:
-        safety_msg = Safety()
-        safety_msg.emergency_state = True
-        safety_msg.bumper_stop = True
-        self.safety_pub.publish(safety_msg)
-
-
+        if msg:
+            self.safety_msg.bumper_stop = True
+        else:
+            self.safety_msg.bumper_stop = False
