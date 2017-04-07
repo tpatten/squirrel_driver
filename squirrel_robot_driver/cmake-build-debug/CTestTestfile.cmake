83e3bed4d7eda89686fd6a52faefa18ed448dfa0
Philipp Zech <philipp.zech@uibk.ac.at>
2017-04-07 09:27:09 +0200

    initial commit for new robot driver/controller

---
+++ squirrel_robot_driver/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/lokalmatador/Work/ROS/squirrel_ws/src/squirrel_driver/squirrel_robot_driver
+# Build directory: /home/lokalmatador/Work/ROS/squirrel_ws/src/squirrel_driver/squirrel_robot_driver/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
