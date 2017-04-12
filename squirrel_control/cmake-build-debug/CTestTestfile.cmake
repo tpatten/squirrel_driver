953601e38837a839f79eb15258f5b62f7d4819d3
Philipp Zech <philipp.zech@uibk.ac.at>
2017-04-12 22:30:43 +0200

    gitignore

--- squirrel_control/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lokalmatador/Work/ROS/squirrel_ws/src/squirrel_driver/squirrel_control
-# Build directory: /home/lokalmatador/Work/ROS/squirrel_ws/src/squirrel_driver/squirrel_control/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- squirrel_control/cmake-build-debug/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/usr/local/include".split(';') if "/usr/local/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;hardware_interface;joint_limits_interface;controller_manager;std_msgs;geometry_msgs;trajectory_msgs;squirrel_safety_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-larm_interface".split(';') if "-larm_interface" != "" else []
-PROJECT_NAME = "squirrel_control"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "0.0.0"
