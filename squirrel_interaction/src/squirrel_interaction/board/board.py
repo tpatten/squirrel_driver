b728dfe41ee4dba51a2e7fd39347762997290547
Henrique Póvoa <hpovoa@idmind.com>
2017-08-09 18:13:52 +0100

    added driver for door motor, tested

--- squirrel_interaction/src/squirrel_interaction/board/board.py
+++ squirrel_interaction/src/squirrel_interaction/board/board.py
@@ -21,19 +21,10 @@
 import rospy
 import serial_api
 from serial import SerialException
-
 from sensor_msgs.msg import JointState
-from std_msgs.msg import Float64, ColorRGBA
-
+from std_msgs.msg import Float64, ColorRGBA, String
+from squirrel_interaction.srv import DoorController
 from math import degrees, radians
-
-
-def _valid(data):
-    return data is not None
-
-
-def _valid_camera(data):
-    return -1.2 < data < 0.53
 
 
 class Controller:
@@ -45,23 +36,35 @@ class Controller:
             self.neck_destination = self._motor.get_position("neck")
             self.camera_destination = self._motor.get_position("camera")
             self.base_lights = [0, 0, 0] * 84 # turns off all base leds
+            self.door_open = False
+            self.door_closed = False
+            self.should_open_door = False
+            self.should_close_door = False
         except SerialException:
             rospy.logwarn('Failed to open some devices')
         rospy.init_node('squirrel_driver', anonymous=False)
         print 'Started squirrel_driver node'
-	rospy.Subscriber('/head_controller/joint_group_position_controller/command', Float64, self.move_head)
+        rospy.Subscriber('/head_controller/joint_group_position_controller/command', Float64, self.move_head)
         rospy.Subscriber('/neck_controller/joint_group_position_controller/command', Float64, self.move_neck)
         rospy.Subscriber('/camera_controller/joint_group_position_controller/command', Float64, self.move_camera)
         rospy.Subscriber('/light/command', ColorRGBA, self.change_base_light)
+        rospy.Service('/door_controller/command', DoorController, self.move_door)
         self.position_pub = rospy.Publisher('/joint_states', JointState, queue_size=10)
 
     def run(self):
-        self.rate = rospy.Rate(50)
+        rate = rospy.Rate(50)
         while not rospy.is_shutdown():
             self._motor.move_to("head", self.head_destination)
             self._motor.move_to("neck", self.neck_destination)
             self._motor.move_to("camera", self.camera_destination)
             self._motor.set_base_led_colors(self.base_lights)
+            door = self._motor.get_door_status()
+            self.door_open = door == 'OPEN'
+            self.door_closed = door == 'CLOSED'
+            if self.should_open_door:
+                self._motor.move_to('door', -20000)
+            if self.should_close_door:
+                self._motor.move_to('door', 30000)
             position = JointState(
                 name=["head"],
                 position=[radians(self._motor.get_position("head"))])
@@ -75,31 +78,39 @@ class Controller:
                 position=[radians(self._motor.get_position("camera"))])
             self.position_pub.publish(position)
 
-            self.rate.sleep()
+            rate.sleep()
 
     def _open_devices(self):
         port = rospy.get_param("/squirrel_port") if rospy.has_param("/squirrel_port") else "/dev/ttyUSB0"
         self._motor = serial_api.Controller(port, 115200)
 
     def move_head(self, message):
-        if _valid(message) and self._motor is not None:
-            self.head_destination = int(degrees(message.data))
+        self.head_destination = int(degrees(message.data))
 
     def move_neck(self, message):
-        if _valid(message) and self._motor is not None:
-            self.neck_destination = int(degrees(message.data))
+        self.neck_destination = int(degrees(message.data))
 
     def move_camera(self, message):
-        if _valid_camera(message) and self._motor is not None:
-            self.camera_destination = int(degrees(message.data))
+        self.camera_destination = int(degrees(message.data))
 
     def change_base_light(self, message):
         color = [int(message.r), int(message.g), int(message.b)]
-        lights = []
-        for x in range(84):
-            lights.extend(color)
-        self.base_lights = lights
+        self.base_lights = color * 42   # number of base leds
 
+    def move_door(self, req):
+        if req.message == 'open':
+            self.should_open_door = True
+            self.should_close_door = False
+            while not self.door_open:
+                pass
+            return True
+        elif req.message == 'close':
+            self.should_open_door = False
+            self.should_close_door = True
+            while not self.door_closed:
+                pass
+            return True
+        return False
 
 if __name__ == '__main__':
     controller = Controller()
--- squirrel_interaction/src/squirrel_interaction/board/serial_api.py
+++ squirrel_interaction/src/squirrel_interaction/board/serial_api.py
@@ -23,13 +23,15 @@ from serial import Serial
 import binascii
 
 _MOTORS = ["head", "neck", "camera", "door"]
+_NUMBER_OF_BASE_LEDS = 42
+_NUMBER_OF_MOUTH_LEDS = 4
 
 
 class Controller:
-    """Main interface for Serial communication with Squirrel Interaction board Motors"""
+    """ Main interface for Serial communication with Squirrel Interaction board """
     def __init__(self, port="/dev/ttyUSB0", baudrate=115200):
         self.serial = Serial(port, baudrate)
-        self.start_base_led_colors(84)
+        self.start_base_led_colors(_NUMBER_OF_BASE_LEDS)
 
     def __del__(self):
         self.serial.close()
@@ -39,13 +41,18 @@ class Controller:
             self.move_to(each_motor, 0)
 
     def move_to(self, motor, degrees):
+        if not _valid_destination(motor, degrees):
+            return 'Invalid destination %d for %s' % (degrees, motor)
         try:
+            message = bytearray(2)
+            message[0] = (degrees >> 8) & 0xff  # simple bitwise operations to split an integer into 2 bytes
+            message[1] = degrees & 0xff
             self.serial.reset_input_buffer()
             self.serial.write([
                 0x30,
                 _MOTORS.index(motor),
-                0 if degrees >= 0 else 255,
-                degrees & 0xff
+                message[0],
+                message[1]
             ])
             response = bytearray()
             response.extend(self.serial.read(5))
@@ -66,36 +73,50 @@ class Controller:
             return - (response[2] - response[3])
 
         except ValueError:
-            return 'Available motors: head, neck, camera, door;'
+            return 'Available motors: head, neck, camera;'
 
     def set_mouth_led_colors(self, colors):
-        if len(colors) != 12:
-            return 'Need RGB values (0-255, 0-255, 0-255) for each Mouth LED (4)'
-        colors_ = map(limit_color, colors)
+        if len(colors) != _NUMBER_OF_MOUTH_LEDS * 3:
+            return 'Need RGB values (0-255, 0-255, 0-255) for each Base LED (%d)' % _NUMBER_OF_MOUTH_LEDS
+        colors_ = map(_limit_color, colors)
         self.serial.reset_input_buffer()
         self.serial.write([0x3A] + colors_)
-        return self.check_response(4, 0x3A)
+        return self._check_response(4, 0x3A)
 
     def start_base_led_colors(self, number_of_leds):
         self.serial.write([0x34, number_of_leds])
-        return self.check_response(4, 0x34)
+        return self._check_response(4, 0x34)
 
     def set_base_led_colors(self, colors):
-        """sets the rgb value for each base led (84 max)"""
-        if len(colors) != 252:
-            return 'Need RGB values (0-255, 0-255, 0-255) for each Mouth LED (4)'
-        colors_ = map(limit_color, colors)
+        """sets the rgb value for each base led (42 max)"""
+        if len(colors) != _NUMBER_OF_BASE_LEDS * 3:
+            return 'Need RGB values (0-255, 0-255, 0-255) for each Base LED (%d)' % _NUMBER_OF_BASE_LEDS
+        colors_ = map(_limit_color, colors)
         self.serial.reset_input_buffer()
         self.serial.write([0x35] + colors_)
-        return self.check_response(4, 0x35)
+        return self._check_response(4, 0x35)
 
-    def check_response(self, number_of_bytes_to_read, should_be):
+    def get_door_status(self):
+        self.serial.reset_input_buffer()
+        self.serial.write([
+            0x55,
+            3
+        ])
+        response = bytearray()
+        response.extend(self.serial.read(6))
+        if response[2] == 0x01:
+            return "OPEN"
+        if response[2] == 0x10:
+            return "CLOSED"
+        return "AJAR"
+
+    def _check_response(self, number_of_bytes_to_read, should_be):
         response = bytearray()
         response.extend(self.serial.read(number_of_bytes_to_read))
         return response[0] == should_be
 
 
-def limit_color(color_value):
+def _limit_color(color_value):
     if color_value > 255:
         return 255
     if color_value < 0:
@@ -103,4 +124,12 @@ def limit_color(color_value):
     return color_value
 
 
+def _valid_destination(motor, degrees):
+    if motor in ['head', 'neck']:
+        return -180 <= degrees <= 180
+    if motor == 'camera':
+        return -60 <= degrees <= 30
+    if motor == 'door':
+        return -20000 <= degrees <= 30000
+    return False
 
