26bff88198334ac4cdc06b6a67e03f242e8602b7
zeno <hpovoa@idmind.pt>
2017-10-10 10:04:59 +0100

    fixed merge with raspberry_screen_server issue

--- squirrel_interaction/src/squirrel_interaction/screen/raspberry_screen_server.py
+++ squirrel_interaction/src/squirrel_interaction/screen/raspberry_screen_server.py
@@ -3,57 +3,13 @@
 """
 
 This ROS Node provides an interface for the Monitor attached to the Raspberry Pi.
-<<<<<<< HEAD
-=======
-All .png files under $(find squirrel_interaction)/resources/media/images are available.
->>>>>>> 8f4f13e531814dcf83efcfb69a3e46804c58860b
+
+The ros param face_color defines subfolder to look into.
+
+All .png files under $(find squirrel_interaction)/resources/media/images/$(face_color) are available.
 
 """
 
-<<<<<<< HEAD
-The service /face/emotion is called with the name of the image to be displayed without extention.
-
-All .png files under $(find squirrel_interaction)/resources/media/images are available.
-
-"""
-
-import os
-import rospy
-import rospkg
-from squirrel_interaction.srv import *
-
-ROSPACK = rospkg.RosPack()
-PATH = ROSPACK.get_path('squirrel_interaction')
-
-def handle_display_on_screen(data):
-    """ If message passed to service is "stop", kill node and EOG.
-
-        If file exists, displays it.
-    """
-    try:
-        base_color = rospy.get_param("face_color", default="navy")
-        if data.message == "stop":
-            os.system("kill $(pidof eog) &")
-            rospy.signal_shutdown('EOG application node closed. Reason: User Command')
-        else:
-            to_open = "%s/resources/media/images/%s/%s.png" % (PATH, base_color, data.message)
-            os.system('eog --fullscreen -w "' + to_open + '" &')
-        return True
-
-    except rospy.ServiceException as exc:
-        print "Service did not process request: " + str(exc)
-        return False
-
-def get_available_expressions(data):
-    """ lists all face names
-    """
-    base_color = rospy.get_param("face_color", default="navy")
-    files_in_dir = os.listdir(PATH + "/resources/media/images/" + base_color)
-    expressions = __filter_png_files(files_in_dir)
-    names = __remove_extensions(expressions)
-    return GetListResponse(names)
-
-=======
 import os
 import cv2
 import rospy
@@ -80,7 +36,6 @@ def get_available_expressions(data):
     names = __remove_extensions(expressions)
     return GetListResponse(names)
 
->>>>>>> 8f4f13e531814dcf83efcfb69a3e46804c58860b
 def __filter_png_files(files):
     for curr_file in files:
         if curr_file.endswith(".png"):
@@ -93,17 +48,12 @@ def __remove_extensions(files):
 def main():
     global img_file
     rospy.init_node('raspberry_screen_server')
-<<<<<<< HEAD
-    rospy.Service('face/emotion', DisplayScreen, handle_display_on_screen)
-    rospy.Service('face/emotions', GetList, get_available_expressions)
-=======
     rospy.Subscriber("/face/emotion", String, emotion_cb)
     rospy.Service('face/emotions', GetList, get_available_expressions)
     cv2.namedWindow("image", cv2.WND_PROP_FULLSCREEN)          
     cv2.setWindowProperty("image", cv2.WND_PROP_FULLSCREEN, cv2.cv.CV_WINDOW_FULLSCREEN)
 
     img_file= "{}/resources/media/images/{}/{}.png".format(PATH, "gold", "look_down")
->>>>>>> 8f4f13e531814dcf83efcfb69a3e46804c58860b
     rospy.loginfo("Ready to display on screen")
 
     r = rospy.Rate(30)
