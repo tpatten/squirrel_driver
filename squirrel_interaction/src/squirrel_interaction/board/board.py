be5e91aefa10ecbf1bea33ef495da8283dbd1c9a
Henrique Póvoa <hpovoa@idmind.com>
2017-11-16 15:06:15 +0000

    more robust communication protocol with interaction board

--- squirrel_interaction/src/squirrel_interaction/board/board.py
+++ squirrel_interaction/src/squirrel_interaction/board/board.py
@@ -12,28 +12,29 @@
 
     Clients may also change motor velocities within certain limits. 
     Limits are as follows:
-	2 <= head_pan <= 30
-	2 <= neck_pan <= 30
-	2 <= neck_tilt <= 25
-	door = 20
+    2 <= head_pan <= 30
+    2 <= neck_pan <= 30
+    2 <= neck_tilt <= 25
+    door = 20
 
     List of topics:
         OUT
             /joint_states [JointState]
         IN
             head_controller/command
-	    head_controller/speed
+            head_controller/speed
             neck_pan_controller/command
-	    neck_pan_controller/speed
+            neck_pan_controller/speed
             neck_tilt_controller/command
-	    neck_tilt_controller/speed
-	    head_and_neck_controller/command
+            neck_tilt_controller/speed
+            head_and_neck_controller/command
             /light/command
-	    /mouth/command
+            /light_complex/command
+            /mouth/command
 
     The door is controlled via a Service.
 
-	    door_controller/command {'open', 'close' }
+            door_controller/command {'open', 'close' }
 
 """
 
@@ -41,7 +42,7 @@ import rospy
 import serial_api
 from serial import SerialException
 from sensor_msgs.msg import JointState
-from std_msgs.msg import Float64, ColorRGBA, String, Header, Float64MultiArray, Int16
+from std_msgs.msg import Float64, ColorRGBA, String, Header, Float64MultiArray, Int16, UInt16MultiArray
 from squirrel_interaction.srv import DoorController
 from math import degrees, radians
 
@@ -52,126 +53,228 @@ class Controller:
         try:
             rospy.init_node('squirrel_driver', anonymous=False)
             self._open_devices()
-            self.head_destination = self._motor.get_position("head")
-            self.neck_destination = self._motor.get_position("neck")
-            self.camera_destination = self._motor.get_position("camera")
-            self.base_lights = [0, 0, 0] * 42 # turns off all base leds
-	    self.mouth_lights = [0, 0, 0] * 4 # turns off all mouth leds
-            self.door_open = False
-            self.door_closed = False
-            #self.should_open_door = False
-            #self.should_close_door = False
+            self.head_position_reference = 0
+            self.neck_pan_position_reference = 0
+            self.neck_tilt_position_reference = 0
+            self.head_speed_reference = None
+            self.neck_pan_speed_reference = None
+            self.neck_tilt_speed_reference = None
+            self.base_lights = None
+            self.mouth_lights = None
+            self.door = 'OPENING'
+            self.to_close_door = False
+            self.to_open_door = True
         except SerialException:
             rospy.logwarn('Failed to open some devices')
         rospy.loginfo('Started squirrel_driver node')
         rospy.Subscriber('head_controller/command', Float64, self.move_head)
-        rospy.Subscriber('neck_pan_controller/command', Float64, self.move_neck)
-        rospy.Subscriber('neck_pan_controller/rel_command', Float64, self.move_neck_rel)
-        rospy.Subscriber('neck_tilt_controller/command', Float64, self.move_camera)
-        rospy.Subscriber('neck_tilt_controller/rel_command', Float64, self.move_camera_rel)
+        rospy.Subscriber('neck_pan_controller/command', Float64, self.move_neck_pan)
+        rospy.Subscriber('neck_pan_controller/rel_command', Float64, self.move_neck_pan_rel)
+        rospy.Subscriber('neck_tilt_controller/command', Float64, self.move_neck_tilt)
+        rospy.Subscriber('neck_tilt_controller/rel_command', Float64, self.move_neck_tilt_rel)
         rospy.Subscriber('head_and_neck_controller/command', Float64MultiArray, self.move_head_and_neck)
-	rospy.Subscriber('head_controller/speed', Int16, self.set_head_speed)
-	rospy.Subscriber('neck_pan_controller/speed', Int16, self.set_neck_pan_speed)
-	rospy.Subscriber('neck_tilt_controller/speed', Int16, self.set_neck_tilt_speed)
+        rospy.Subscriber('head_controller/speed', Int16, self.set_head_speed)
+        rospy.Subscriber('neck_pan_controller/speed', Int16, self.set_neck_pan_speed)
+        rospy.Subscriber('neck_tilt_controller/speed', Int16, self.set_neck_tilt_speed)
 
-	rospy.Subscriber('/light/command', ColorRGBA, self.change_base_light)
-	rospy.Subscriber('/mouth/command', ColorRGBA, self.change_mouth_light)
+        rospy.Subscriber('/light/command', ColorRGBA, self.change_base_light)
+        rospy.Subscriber('/light_complex/command', UInt16MultiArray, self.change_base_light_complex)
+        rospy.Subscriber('/mouth/command', ColorRGBA, self.change_mouth_light)
         rospy.Service('door_controller/command', DoorController, self.move_door)
         self.position_pub = rospy.Publisher('/joint_states', JointState, queue_size=10)
+        self.door_state_pub = rospy.Publisher('/door_state', String, queue_size=10)
 
     def run(self):
         rate = rospy.Rate(20)
-	last_correct_positions = [0.0, 0.0, 0.0, 0.0]
+        last_correct_positions = [0.0, 0.0, 0.0, 0.0]
         while not rospy.is_shutdown():
+            #  READ
             joint_state_msg = JointState()
             joint_state_msg.header = Header()
             joint_state_msg.header.stamp = rospy.Time.now()
             joint_state_msg.name = ['head_joint', 'neck_pan_joint', 'neck_tilt_joint', 'door_joint']
-	    positions = self._motor.get_all_positions()
-	    if positions:
-            	joint_state_msg.position = map(radians, positions)
-		last_correct_positions = joint_state_msg.position
-	    else:
-		rospy.loginfo('incorrect motor position reads. using last known correct')
-		joint_state_msg.position = last_correct_positions
-	    joint_state_msg.velocity = []
-      	    joint_state_msg.effort = []
+            positions = self._motor.get_all_positions()
+            if positions:
+                joint_state_msg.position = map(radians, positions)
+                last_correct_positions = joint_state_msg.position
+            else:
+                joint_state_msg.position = last_correct_positions
+                joint_state_msg.velocity = []
+                joint_state_msg.effort = []
             self.position_pub.publish(joint_state_msg)
+
+            door = self._motor.get_door_status()
+            #if door in ['OPEN', 'CLOSED', 'AJAR']:
+            #    self.door = door
+            #    self.door_state_pub.publish(door)
+            if door == 'OPEN' and self.door == 'OPENING':
+                self.door = 'OPEN'
+            if door == 'CLOSED' and self.door == 'CLOSING':
+                self.door = 'CLOSED'
+            self.door_state_pub.publish(self.door)
+            #  WRITE
+            #  speeds
+            
+            if self.head_speed_reference is not None:
+                if self._motor.set_motor_speed('head', self.head_speed_reference):
+                    self.head_speed_reference = None
+            if self.neck_pan_speed_reference is not None:
+                if self._motor.set_motor_speed('neck', self.neck_pan_speed_reference):
+                    self.neck_pan_speed_reference = None
+            if self.neck_tilt_speed_reference is not None:
+                if self._motor.set_motor_speed('camera', self.neck_tilt_speed_reference):
+                    self.neck_tilt_speed_reference = None
+
+            #  other motors
+            #########################################################################
+            #self._motor.move_all(
+            #    self.head_position_reference,
+            #    self.neck_pan_position_reference,
+            #    self.neck_tilt_position_reference
+            #)
+            #########################################################################
+            if self.head_position_reference is not None:
+                if self._motor.move_to('head', self.head_position_reference):
+                    self.head_position_reference = None
+            if self.neck_pan_position_reference is not None:
+                if self._motor.move_to('neck', self.neck_pan_position_reference):
+                    self.neck_pan_position_reference = None
+            if self.neck_tilt_position_reference is not None:
+                if self._motor.move_to('camera', self.neck_tilt_position_reference):
+                    self.neck_tilt_position_reference = None
+
+            #  door
+            if self.to_close_door:
+                self.door = 'CLOSING'
+                if self._motor.move_to('door', 30000):
+                    self.to_close_door = False
+
+            if self.to_open_door:
+                self.door = 'OPENING'
+                if self._motor.move_to('door', -30000):
+                    self.to_open_door = False
+            # lights
+            if self.mouth_lights:
+                if self._motor.set_mouth_led_colors(self.mouth_lights):
+                    self.mouth_lights = None
+            
+            if self.base_lights:
+                if self._motor.set_base_led_colors(self.base_lights):
+                    self.base_lights = None
+
             rate.sleep()
 
     def _open_devices(self):
         port = rospy.get_param("/squirrel_port") if rospy.has_param("/squirrel_port") else "/dev/ttyBoard"
-        self._motor = serial_api.Controller(port, 115200, timeout=0.02)
+        self._motor = serial_api.Controller(port, 115200, timeout=0.05)
 
     def set_head_speed(self, message):
-	self._motor.set_motor_speed("head", message.data)
+        speed = message.data
+        if not self._motor.set_motor_speed('head', speed): 
+            self.head_speed_reference = speed
+        else:
+            self.head_speed_reference = None
 
     def set_neck_pan_speed(self, message):
-        self._motor.set_motor_speed("neck", message.data)
+        speed = message.data
+        if not self._motor.set_motor_speed('neck', speed):
+            self.neck_pan_speed_reference = speed
+        else:
+            self.neck_pan_speed_reference = None
 
     def set_neck_tilt_speed(self, message):
-        self._motor.set_motor_speed("camera", message.data)
+        speed = message.data
+        if not self._motor.set_motor_speed('camera', speed):
+            self.neck_tilt_speed_reference = message.data
+        else:
+            self.neck_tilt_speed_reference = None
 
     def move_head(self, message):
-        self.head_destination = int(degrees(message.data))
-        self._motor.move_to("head", self.head_destination)
+        reference = int(degrees(message.data))
+        if not self._motor.move_to('head', reference):
+            self.head_position_reference = int(degrees(message.data))
+        else:
+            self.head_position_reference = None
 
-    def move_neck(self, message):
-        rospy.loginfo("received {} on move_neck".format(message.data))
-        self.neck_destination = int(degrees(message.data))
-        self._motor.move_to("neck", self.neck_destination)
+    def move_neck_pan(self, message):
+        reference = int(degrees(message.data))
+        if not self._motor.move_to('neck', reference):
+            self.neck_pan_position_reference = int(degrees(message.data))
+        else:
+            self.neck_pan_position_reference = None
 
-    def move_neck_rel(self, message):
+    def move_neck_pan_rel(self, message):
         position = self._motor.get_position("neck")
-        print("current neck position: {}".format(position))
-        self.neck_destination = position + int(degrees(message.data))
-        print("next neck position: {}".format(self.neck_destination))
-        self._motor.move_to("neck", self.neck_destination)
+        reference = position + int(degrees(message.data))
+        if not self._motor.move_to('neck', reference):
+            self.neck_pan_position_reference = reference
+        else:
+            self.neck_pan_position_reference = None
 
-    def move_camera(self, message):
-        self.camera_destination = int(degrees(message.data))
-        self._motor.move_to("camera", self.camera_destination)
+    def move_neck_tilt(self, message):
+        reference = int(degrees(message.data))
+        if not self._motor.move_to('camera', reference):
+            self.neck_tilt_position_reference = reference
+        else:
+            self.neck_tilt_position_reference = None
 
-    def move_camera_rel(self, message):
+    def move_neck_tilt_rel(self, message):
         position = self._motor.get_position("camera")
-        print("current camera position: {}".format(position))
-        self.camera_destination = position + int(degrees(message.data))
-        print("next camera position: {}".format(self.camera_destination))
-        self._motor.move_to("camera", self.camera_destination)
+        reference = int(degrees(message.data))
+        if not self._motor.move_to('camera', reference):
+            self.neck_tilt_position_reference = reference
+        else:
+            self.neck_tilt_position_reference = None
 
     def move_head_and_neck(self, message):
-	""" moves head_pan, neck_pan and neck_tilt to message.data[0, 1, 2] """
-        self.head_destination = int(degrees(message.data[0]))
-	self.neck_destination = int(degrees(message.data[1]))
-	self.camera_destination = int(degrees(message.data[2]))
-	self._motor.move_all(self.head_destination, self.neck_destination, self.camera_destination)
+        """ moves head_pan, neck_pan and neck_tilt to message.data[0, 1, 2] """
+        head_reference = int(degrees(message.data[0]))
+        neck_pan_reference = int(degrees(message.data[1]))
+        neck_tilt_reference = int(degrees(message.data[2]))
+        if not self._motor.move_all(head_reference, neck_pan_reference, neck_tilt_reference):
+            self.head_position_reference = head_reference
+            self.neck_pan_position_reference = neck_pan_reference
+            self.neck_tilt_position_reference = neck_tilt_reference
+        else:
+            self.head_position_reference = None
+            self.neck_pan_position_reference = None
+            self.neck_tilt_position_reference = None
 
     def change_base_light(self, message):
-        color = [int(message.r), int(message.g), int(message.b)]
-        self.base_lights = color * 42   # number of base leds
-        self._motor.set_base_led_colors(self.base_lights)
+        color = [int(message.r), int(message.g), int(message.b)] * 42
+        if not self._motor.set_base_led_colors(colors):
+            self.base_lights = color
+        else:
+            self.base_lights = None
+
+    def change_base_light_complex(self, message):
+        colors = message.data
+        if len(colors) != 126:
+            rospy.logwarn('failed to send complex light command. need RGB component for each led (42 * 3 = 126')
+        self.base_lights = colors
+        #else:
+        #    if not self._motor.set_base_led_colors(colors):
+        #        self.base_lights = colors
+        #    else:
+        #        self.base_lights = None
 
     def change_mouth_light(self, message):
-        color = [int(message.r), int(message.g), int(message.b)]
-        self.mouth_lights = color * 4   # number of base leds
-        self._motor.set_mouth_led_colors(self.mouth_lights)
+        color = [int(message.r), int(message.g), int(message.b)] * 4
+        if not self._motor.set_mouth_led_colors(color):
+            self.mouth_lights = color   # number of base leds
+        else:
+            self.mouth_lights = None
 
     def move_door(self, req):
         if req.message == 'open':
-            self._motor.move_to('door', -20000)
-            while not self.door_open:
-                pass
+            self.to_open_door = True
             return True
         elif req.message == 'close':
-            self._motor.move_to('door', 30000)
-            while not self.door_closed:
-                pass
+            self.to_close_door = True
             return True
         return False
+
 
 if __name__ == '__main__':
     controller = Controller()
     controller.run()
-
-
-
--- squirrel_interaction/src/squirrel_interaction/board/serial_api.py
+++ squirrel_interaction/src/squirrel_interaction/board/serial_api.py
@@ -17,17 +17,18 @@ The board controls the following devices:
 
     - 4 mouth RGB Led array
 
+All public methods in this module return False if communication has failed
+
 """
 
 import time
 from serial import Serial, SerialException
-import binascii
 from threading import Lock
 import rospy
 
 _MOTORS = ["head", "neck", "camera", "door"]
 # _MOTOR_SPEEDS list: (min_speed current_speed max_speed)
-_MOTOR_SPEEDS = [[2, 10, 30], [2, 15, 30], [2, 10, 25], [20, 20, 20]]
+_MOTOR_SPEEDS = [[0, 10, 30], [0, 15, 30], [0, 10, 25], [100, 100, 100]]
 _NUMBER_OF_BASE_LEDS = 42
 _NUMBER_OF_MOUTH_LEDS = 4
 
@@ -58,6 +59,7 @@ class Controller(object):
     def move_to(self, motor, degrees):
         """ Moves motor [param 0] to position [param 1] degrees """
         degrees = _valid_destination(motor, degrees)
+        response = False
         self._mutex.acquire()
         try:
             message = bytearray(2)
@@ -65,14 +67,13 @@ class Controller(object):
             message[0] = (degrees >> 8) & 0xff
             message[1] = degrees & 0xff
             self.serial.reset_input_buffer()
-            rospy.loginfo("sending movement command")
             self.serial.write([
                 0x30,
                 _MOTORS.index(motor),
                 message[0],
                 message[1]
             ])
-	    response = self._check_response(5, 0x30)
+            response = self._check_response(5, 0x30)
             rospy.loginfo("movement command done")
 
         except ValueError:
@@ -81,160 +82,171 @@ class Controller(object):
             rospy.loginfo('Unable to access serial port. Is it still in use?')
         finally:
             self._mutex.release()
-            return response
+        return response
 
     def move_all(self, head, neck, camera):
-	head = _valid_destination("head", head)
-	neck = _valid_destination("neck", neck)
-	camera = _valid_destination("camera", camera)
-	self._mutex.acquire()
+        head = _valid_destination("head", head)
+        neck = _valid_destination("neck", neck)
+        camera = _valid_destination("camera", camera)
+        response = False
+        self._mutex.acquire()
         try:
             message = bytearray(6)
             # simple bitwise operations to split an integer into 2 bytes
             message[0] = (head >> 8) & 0xff
             message[1] = head & 0xff
-	    message[2] = (neck >> 8) & 0xff
+            message[2] = (neck >> 8) & 0xff
             message[3] = neck & 0xff
-	    message[4] = (camera >> 8) & 0xff
+            message[4] = (camera >> 8) & 0xff
             message[5] = camera & 0xff
             self.serial.reset_input_buffer()
-            rospy.loginfo("sending movement command")
             self.serial.write([
                 0x3B,
                 message[0],
                 message[1],
-		message[2],
-		message[3],
-		message[4],
-		message[5]
+                message[2],
+                message[3],
+                message[4],
+                message[5]
             ])
             response = self._check_response(4, 0x3B)
-            rospy.loginfo("movement command done")
 
         except ValueError:
-    	    return 'Available motors: head, neck, camera, door;'
+            return 'Available motors: head, neck, camera, door;'
         except SerialException:
             rospy.loginfo('Unable to access serial port. Is it still in use?')
         finally:
             self._mutex.release()
-            return response
+        return response
 
     def start_motors(self):
         """ Sets initial speeds for all Motors """
-	self.reset()
+        self.reset()
         for motor in _MOTORS:
             self.start_motor(motor)
 
     def stop_motor(self, motor):
         """ Stops motor [param 0] """
+        response = False
         try:
-	    self._mutex.acquire()
+            self._mutex.acquire()
             self.serial.reset_input_buffer()
-	    self.serial.write([0x31, _MOTORS.index(motor), 0, 0])
+            self.serial.write([0x31, _MOTORS.index(motor), 0, 0])
             response = self._check_response(5, 0x31)
         except ValueError:
-            return 'Available motors: head, neck, camera, door;'
-	finally:
-	    self._mutex.release()
-            return response
+            rospy.loginfo('Available motors: head, neck, camera, door;')
+        finally:
+            self._mutex.release()
+
+        return response
 
     def start_motor(self, motor):
         """ Allows motor [param 0] to move again. By default, motors can move """
+        response = False
         try:
             index = _MOTORS.index(motor)
-	    self._mutex.acquire()
+            speed = _MOTOR_SPEEDS[index][1]
+            self._mutex.acquire()
             self.serial.reset_input_buffer()
-	    self.serial.write([0x31, index, 0, _MOTOR_SPEEDS[index][1]])
+            self.serial.write([0x31, index, 0, speed])
             response = self._check_response(5, 0x31)
         except ValueError:
-            return 'Available motors: head, neck, camera, door;'
-	finally:
-	    self._mutex.release()
-            return response
+            rospy.loginfo('Available motors: head, neck, camera, door;')
+        finally:
+            self._mutex.release()
+        return response
 
     def set_motor_speed(self, motor, speed):
-	    i = _MOTORS.index(motor)
-	    if speed >= _MOTOR_SPEEDS[i][0] and speed <= _MOTOR_SPEEDS[i][2]:
-		_MOTOR_SPEEDS[i][1] = speed
-		self.start_motor(motor)
+        i = _MOTORS.index(motor)
+        # if speed is out of range, changes nothing
+        if _MOTOR_SPEEDS[i][0] <= speed <= _MOTOR_SPEEDS[i][2]:
+            _MOTOR_SPEEDS[i][1] = speed
+        return self.start_motor(motor)
 
     def set_all_motor_speeds(self, head_speed, neck_speed, camera_speed):
-	    self.set_motor_speed("head", head_speed)
-	    self.set_motor_speed("neck", neck_speed)
-	    self.set_motor_speed("camera", camera_speed)
+        self.set_motor_speed("head", head_speed)
+        self.set_motor_speed("neck", neck_speed)
+        self.set_motor_speed("camera", camera_speed)
 
     def get_position(self, motor):
         """ Get motor [param 0] position in degrees """
+        response = False
         self._mutex.acquire()
         try:
             self.serial.reset_input_buffer()
-	    self.serial.write([
+            self.serial.write([
                 0x52,
                 _MOTORS.index(motor)
             ])
-            response = bytearray(self.serial.read(7))
+            response = bytearray()
+            response.extend(self.serial.read(7))
+            if len(response) != 7 or response[0] != 0x52:
+                response = False
         except ValueError:
-            return 'Available motors: head, neck, camera;'
+            rospy.loginfo('Available motors: head, neck, camera;')
         except SerialException:
             rospy.loginfo('Unable to access serial port. Is it still in use?')
         finally:
             self._mutex.release()
-            return - (response[2] - response[3])
+        if not response:
+            return response
+        return - (response[2] - response[3])
 
     def get_all_positions(self):
         """ Fetches all motor positions """
-	self._mutex.acquire()
-	success = True
-	try:
+        response = False
+        self._mutex.acquire()
+        try:
             self.serial.reset_input_buffer()
             self.serial.write([0x5C])
             response = self.serial.read(10)
-	    if len(response) != 10 or ord(response[0]) != 0x5c:
-                success = False
+            if len(response) != 10 or ord(response[0]) != 0x5c:
+                response = False
         except ValueError:
             rospy.loginfo('Available motors: head, neck, camera;')
-	    success = False
         except SerialException:
             rospy.loginfo('Unable to access serial port. Is it still in use?')
-	    success = False
         finally:
             self._mutex.release()
-	    if not success:
-		return success
-	    return [_to_num(response[1:3]), 
-		    _to_num(response[3:5]), 
-	  	    _to_num(response[5:7]),
-		    self.get_position("door")]
+        if not response:
+            return response
+        return [_to_num(response[1:3]),
+                _to_num(response[3:5]),
+                _to_num(response[5:7]),
+                self.get_position("door")]
 
     def set_mouth_led_colors(self, colors):
         """ Applies the specified RGBA value to all 4 mouth leds """
         if len(colors) != _NUMBER_OF_MOUTH_LEDS * 3:
             return 'Need RGB values (0-255, 0-255, 0-255) for each Base LED (%d)' % _NUMBER_OF_MOUTH_LEDS
         colors_ = map(_limit_color, colors)
+        response = False
         self._mutex.acquire()
         try:
             self.serial.reset_input_buffer()
             self.serial.write([0x3A] + colors_)
-	    response = self._check_response(4, 0x3A)
+            response = self._check_response(4, 0x3A)
         except SerialException:
             rospy.loginfo('Unable to access serial port. Is it still in use?')
         finally:
             self._mutex.release()
-            return response
+        return response
 
     def start_base_led_colors(self, number_of_leds):
+        response = False
         self._mutex.acquire()
         try:
             self.serial.write([0x34, number_of_leds])
-	    response = self._check_response(4, 0x34)
+            response = self._check_response(4, 0x34)
         except SerialException:
             rospy.loginfo('Unable to access serial port. Is it still in use?')
         finally:
             self._mutex.release()
-            return response
+        return response
 
     def set_base_led_colors(self, colors):
         """sets the rgb value for each base led (42 max)"""
+        response = False
         if len(colors) != _NUMBER_OF_BASE_LEDS * 3:
             return 'Need RGB values (0-255, 0-255, 0-255) for each Base LED (%d)' % _NUMBER_OF_BASE_LEDS
         colors_ = map(_limit_color, colors)
@@ -247,8 +259,10 @@ class Controller(object):
             rospy.loginfo('Unable to access serial port. Is it still in use?')
         finally:
             self._mutex.release()
+            return response
 
     def get_door_status(self):
+        response = False
         self._mutex.acquire()
         try:
             self.serial.reset_input_buffer()
@@ -258,7 +272,9 @@ class Controller(object):
             ])
             response = bytearray()
             response.extend(self.serial.read(6))
-            if response[2] == 0x01:
+            if len(response) != 6 or response[0] != 0x55:
+                response = False
+            elif response[2] == 0x01:
                 response = "OPEN"
             elif response[2] == 0x10:
                 response = "CLOSED"
@@ -268,7 +284,7 @@ class Controller(object):
             rospy.loginfo('Unable to access serial port. Is it still in use?')
         finally:
             self._mutex.release()
-            return response
+        return response
 
     def _check_response(self, number_of_bytes_to_read, should_be):
         try:
@@ -277,6 +293,9 @@ class Controller(object):
             return response[0] == should_be
         except SerialException:
             rospy.loginfo('Unable to access serial port. Is it still in use?')
+        except IndexError:
+            rospy.loginfo('Response inconsistent')
+        return False
 
 
 def _limit_color(color_value):
@@ -292,8 +311,9 @@ def _valid_destination(motor, degrees):
         return max(-30000, min(30000, degrees))
     return False
 
+
 def _to_num(bval):
     res = (ord(bval[0]) << 8) | (ord(bval[1]) & 0xFF)
-    if res > 32767: res = res - 65536
+    if res > 32767:
+        res = res - 65536
     return res
-
